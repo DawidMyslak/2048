4a5842a88f8041fe61e41ba6064291897a050b0a
Dawid Myslak <dawid.myslak@teamwork.com>
2023-02-17 16:52:17 +0100

    Button styling

--- src/components/GameDashboard.vue
+++ src/components/GameDashboard.vue
@@ -1,28 +1,36 @@
 <template>
   <div class="game-dashboard">
     <div class="score">SCORE: {{ state.score }}</div>
-    <div>
+    <div class="row">
       GRID:
-      <select v-model="gameConfig.gridSize">
-        <option value="4">4x4</option>
-        <option value="5">5x5</option>
-        <option value="6">6x6</option>
-        <option value="7">7x7</option>
-        <option value="8">8x8</option>
-      </select>
+      <button
+        v-for="option in gridOptions"
+        class="button"
+        :class="{ 'button-active': gameConfig.gridSize === option.value }"
+        :key="option.value"
+        @click="gameConfig.gridSize = option.value"
+      >
+        {{ option.label }}
+      </button>
     </div>
-    <div>
+    <div class="row">
       OBSTACLES:
-      <select v-model="gameConfig.numberOfObstacles">
-        <option value="0">0</option>
-        <option value="1">1</option>
-        <option value="2">2</option>
-        <option value="3">3</option>
-        <option value="4">4</option>
-      </select>
+      <button
+        v-for="option in obstaclesOptions"
+        class="button"
+        :class="{
+          'button-active': gameConfig.numberOfObstacles === option.value,
+        }"
+        :key="option.value"
+        @click="gameConfig.numberOfObstacles = option.value"
+      >
+        {{ option.label }}
+      </button>
     </div>
     <div>
-      <button @click="$emit('click:newGame', gameConfig)">NEW GAME</button>
+      <button class="button" @click="$emit('click:newGame', gameConfig)">
+        NEW GAME
+      </button>
     </div>
   </div>
 </template>
@@ -38,13 +46,61 @@ const gameConfig = reactive({
   gridSize: DEFAULT_GRID_SIZE,
   numberOfObstacles: DEFAULT_NUMBER_OF_OBSTACLES,
 });
+
+const gridOptions = [
+  { value: 4, label: "4x4" },
+  { value: 5, label: "5x5" },
+  { value: 6, label: "6x6" },
+  { value: 7, label: "7x7" },
+  { value: 8, label: "8x8" },
+];
+
+const obstaclesOptions = [
+  { value: 0, label: "0" },
+  { value: 1, label: "1" },
+  { value: 2, label: "2" },
+  { value: 3, label: "3" },
+  { value: 4, label: "4" },
+];
 </script>
 
 <style scoped>
+.game-dashboard {
+  border-radius: 24px;
+  background-color: #f1f1f1;
+  padding: 12px;
+  margin-bottom: 12px;
+}
+
 .score {
   font-weight: bold;
   color: #000;
   font-size: 32px;
-  padding: 0 8px 8px;
+}
+
+button {
+  font-family: inherit;
+}
+
+.button {
+  min-width: 44px;
+  font-size: 14px;
+  font-weight: bold;
+  padding: 4px 6px;
+  margin: 0 4px 0 0;
+  cursor: pointer;
+  background-color: #fff;
+  border: 2px solid #888;
+  border-radius: 8px;
+  color: #000;
+}
+
+.button-active {
+  background-color: #888;
+  color: #fff;
+}
+
+.row {
+  margin-bottom: 10px;
 }
 </style>
